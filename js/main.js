00feab35afc354a8e1657ce8b0bc373d24e7be3c
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-03 11:25:52 +0900

    新規作成

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// const num_1 = 10;
+// const num_2 = 3;
+// const num_3 = 2;
+// const num_4 = 10000;
+
+// console.log(`${num_1} + ${num_2} = ${num_1 + num_2}`);
+// console.log(`${num_1} - ${num_2} = ${num_1 - num_2}`);
+// console.log(`${num_1} * ${num_2} = ${num_1 * num_2}`);
+// console.log(`${num_4} / ${num_3} = ${num_4 / num_3}`);
+// console.log(`${num_1} % ${num_2} =${num_1} / ${num_2} 余り ${num_1 % num_2}`);
+// console.log(`2の2乗 = ${2 ** 2}`);
+// console.log(`2の3乗 = ${2 ** 3}`);
+
+// for (let i = 0; i <= 30; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 25; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// const module = (num) => {
+//   for (let i = 1; i <= 25; i++) {
+//     if (num > 25) {
+//       console.log(num);
+//     } else if (i % num === 0) {
+//       console.log(i);
+//     }
+//   }
+// };
+
+// module(5);
+// module(7);
+// module(26);
+
+// const checkAlcoholIf = (age) => {
+//   const message = age >= 20 ? "OK" : "NG";
+//   if (age >= 20) {
+//     console.log(`あなたの場合は、お酒げ${message}です`);
+//   } else {
+//     console.log(`あなたの場合は、お酒げ${message}です`);
+//   }
+// };
+
+// checkAlcoholIf(10);
+// checkAlcoholIf(19);
